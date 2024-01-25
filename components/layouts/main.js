8dfe6bee8da002966a863176302ae9876427d7e6
Dexterzprotege <sharath.kamlekar@outlook.com>
2024-01-25 21:43:00 +0100

    Warnings supress

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,22 +1,7 @@
 import Head from 'next/head'
-import dynamic from 'next/dynamic'
 import Navbar from '../navbar.js'
-import { Box, Container, Image } from '@chakra-ui/react'
-import VoxelComputerLoader from '../voxel-computer-loader'
+import { Box, Container } from '@chakra-ui/react'
 import Footer from '../footer'
-import styled from '@emotion/styled'
-
-const Img = styled(Image)`
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-`
-
-// eslint-disable-next-line no-unused-vars
-const LazyVoxelComputer = dynamic(() => import('../voxel-computer'), {
-  ssr: false,
-  loading: () => <VoxelComputerLoader />
-})
 
 const Main = ({ children, router }) => {
   return (
@@ -29,8 +14,6 @@ const Main = ({ children, router }) => {
       <Navbar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
-        {/* <LazyVoxelComputer /> */}
-        {/* <Img src="/banner.png" alt="Banner Cover" boxSize="sm" align="center" /> */}
         {children}
         <Footer />
       </Container>
